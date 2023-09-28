b890d04861ae1ced2b505e1a83c1e78e0567fe6b
sscracker <vova.grigfd@gmail.com>
2023-09-28 18:52:02 +0400

    feature rm/8 2.1

--- app/src/main/java/ru/example/rickandmortyproject/Application.kt
+++ app/src/main/java/ru/example/rickandmortyproject/Application.kt
@@ -6,11 +6,14 @@ import ru.example.rickandmortyproject.di.App
 
 class Application : Application(), App {
     private var appComponent: AppComponent? = null
+
     override fun appComponent() = getAppComponent()
+
     override fun onCreate() {
         super.onCreate()
         appComponent()
     }
+
     private fun getAppComponent(): AppComponent{
         if (appComponent == null){
             appComponent = AppComponent.init(applicationContext)
